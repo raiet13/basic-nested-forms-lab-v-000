13125acaf7d5c77a92e5806768a1c19443c17fa9
Raiet Intraub <raietsaeed@gmail.com>
2018-06-01 23:27:38 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,5 +12,14 @@ class RecipesController < ApplicationController
   end
 
   def create
+    Recipe.create(recipe_params)
+    redirect_to reciple_path
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title)
+  end
+
 end
